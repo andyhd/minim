d6da28fdd01687d70c40e26449ce64f5475d199f
Andy Driver <andy.driver@gcapmedia.com>
2008-09-09 20:02:27 +0100

    Default admin template

---
+++ templates/admin/default.php
@@ -0,0 +1,14 @@
+<?php minim()->extend('base') ?>
+
+<?php minim()->def_block('title') ?>Admin<?php minim()->end_block('title') ?>
+
+<?php minim()->def_block('body_class') ?>admin<?php minim()->end_block('body_class') ?>
+
+<?php minim()->def_block('page_content') ?>
+    <h1>Admin Site</h1>
+    <ul class="messages">
+    <?php foreach (minim()->user_messages() as $msg): ?>
+      <li><?php echo $msg ?></li>
+    <?php endforeach ?>
+    </ul>
+<?php minim()->end_block('page_content') ?>

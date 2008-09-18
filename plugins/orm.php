3098d9042cc30a4f6bac7a8a27d5e007b59d7c9e
Andy Driver <andy.driver@gcapmedia.com>
2008-09-18 18:27:59 +0100

    ModelSets now implement Iterator and use lazy loading on access

--- plugins/orm.php
+++ plugins/orm.php
@@ -45,22 +45,14 @@
 
 class Minim_DataObject // {{{
 {
-    var $_type;
     var $_name;
     var $_fields;
-    var $_errors;
-    var $_validated;
-    var $_json;
     var $_default_sort;
 
     function Minim_DataObject($name) // {{{
     {
-        $this->_type = NULL;
         $this->_name = $name;
         $this->_fields = array();
-        $this->_errors = array();
-        $this->_validated = FALSE;
-        $this->_json = NULL;
     } // }}}
 
     function _setFields(&$fields) // {{{
@@ -69,7 +61,7 @@ class Minim_DataObject // {{{
         foreach ($fields as $name => $field)
         {
             $clone =& $field->copy();
-            if ($field->type == "slug")
+            if ($field->_type == "slug")
             {
                 $from = $clone->getAttribute('from');
                 if (is_null($from) or !array_key_exists($from, $this->_fields))
@@ -95,10 +87,6 @@ class Minim_DataObject // {{{
             if ($ret === FALSE)
             {
                 minim('log')->debug("Couldn't set $name to ".print_r($value, TRUE));
-            }
-            else
-            {
-                $this->_validated = FALSE;
             }
             return $ret;
         }
@@ -155,35 +143,14 @@ class Minim_DataObject // {{{
         return $this;
     } // }}}
 
-    function isValid() // {{{
-    {
-        if (!$this->_validated)
-        {
-            $this->_validated = TRUE;
-            $errors = array();
-            foreach ($this->_fields as $name => $field)
-            {
-                $errors[] = $field->isValid() ? NULL : "Field $name invalid";
-            }
-            $errors = array_filter($errors);
-            $this->_errors = $errors;
-        }
-        return empty($this->_errors);
-    } // }}}
-
-    function errors() // {{{
-    {
-        return $this->_errors;
-    } // }}}
-
     function save() // {{{
     {
-        breve($this->_name)->save($this);
+        minim('orm')->{$this->_name}->save($this);
     } // }}}
 
     function delete() // {{{
     {
-        breve($this->_name)->delete($this);
+        minim('orm')->{$this->_name}->delete($this);
     } // }}}
 
     function to_array() // {{{
@@ -213,7 +180,7 @@ class Minim_Orm_Field // {{{
     function &copy() // {{{
     {
         $class = get_class($this);
-        $clone =& new $class(array_filter($this->_attrs));
+        $clone =& new $class($this->_type, array_filter($this->_attrs));
         return $clone;
     } // }}}
 
@@ -552,6 +519,373 @@ class Minim_Orm_Manager // {{{
     } // }}}
 } // }}}
 
+class Minim_Orm_ModelSet implements Iterator // {{{
+{
+    var $_model;
+    var $_filters;
+    var $_sorting;
+    var $_start;
+    var $_num;
+    var $_cache;
+    var $_count;
+    var $_filter_classes;
+
+    function Minim_Orm_ModelSet($model) // {{{
+    {
+        $this->_table = minim('orm')->{$model}->table();
+        $this->_model = $model;
+        $this->_filters = array();
+        $this->_sorting = array();
+        $this->_start = 0;
+        $this->_num = 0;
+        $this->_cache = array();
+        $this->_count = NULL;
+        $this->_filter_classes = $this->get_filter_classes();
+        $this->_iterator_position = 0;
+    } // }}}
+
+    function filter($kwargs=array()) // {{{
+    {
+        foreach ($kwargs as $key => $arg)
+        {
+            // can't check whether model contains specified fields until we
+            // instantiate, but waiting til then might be good anyway.
+            // filters are ANDed together
+            list($field, $op) = explode('__', $key);
+            $class = $this->_class_for($op, $arg);
+            $this->_filters[] =& new $class($field, $op, $arg);
+        }
+        return $this;
+    } // }}}
+
+    function order_by() // {{{
+    {
+        $args = func_get_args();
+        foreach ($args as $arg)
+        {
+            preg_match('/^([-+?])([a-zA-Z0-9_]+)$/', $arg, $m);
+            if ($m)
+            {
+                $direction = $m[1];
+                $field = $m[2];
+            }
+            else
+            {
+                $direction = '+';
+                $field = $arg;
+            }
+            $this->_sorting[] = array($field, $direction);
+        }
+        return $this;
+    } // }}}
+
+    function limit($a, $b=0) // {{{
+    {
+        if ($b)
+        {
+            $this->_start = $a;
+            $this->_num = $b;
+        }
+        else
+        {
+            $this->_start = 0;
+            $this->_num = $a;
+        }
+        return $this;
+    } // }}}
+
+    function get_filter_classes() // {{{
+    {
+        static $classes = array();
+        if (!$classes)
+        {
+            // find all available filter types
+            foreach (get_declared_classes() as $class)
+            {
+                if (is_subclass_of($class, 'Minim_Orm_Filter'))
+                {
+                    $func = array($class, "register");
+                    $reg = call_user_func($func);
+                    foreach ($reg as $op => $class)
+                    {
+                        $classes[$op] = $class;
+                    }
+                }
+            }
+        }
+        return $classes;
+    } // }}}
+
+    function _class_for($op, $arg) // {{{
+    {
+        // find filter class for $key
+        $classes = $this->get_filter_classes();
+        if (!array_key_exists($op, $classes))
+        {
+            return 'Minim_Orm_Filter';
+        }
+        return $classes[$op];
+    } // }}}
+
+    function count() // {{{
+    {
+        if (is_null($this->_count))
+        {
+            list($query, $params) = $this->build_count_query();
+            $s = $this->execute_query($query, $params);
+            $row = $s->fetch();
+            if (!($this->_count = @$row['_total']))
+            {
+                $this->_count = 0;
+            }
+        }
+        return $this->_count;
+    } // }}}
+
+    function _fill_cache() // {{{
+    {
+        list($query, $params) = $this->build_query();
+        $s = $this->execute_query($query, $params);
+        $this->_cache = $this->_results_to_objects($s);
+    } // }}}
+
+    var $_max_existing = array();
+
+    function _disambiguate_params($params, $fparams, $fquery) // {{{
+    {
+        $fkeys = array_keys($fparams);
+        $pkeys = array_keys($params);
+        foreach ($fkeys as &$key)
+        {
+            if (in_array($key, $pkeys))
+            {
+                if (!array_key_exists($key, $this->_max_existing))
+                {
+                    $this->_max_existing[$key] = 0;
+                }
+                $this->_max_existing[$key]++;
+                $new_key = "{$key}{$this->_max_existing[$key]}";
+                $fquery = str_replace($key, $new_key, $fquery);
+                $fparams[$new_key] = $fparams[$key];
+                unset($fparams[$key]);
+            }
+        }
+        return array($fquery, $fparams);
+    } // }}}
+
+    function build_count_query() // {{{
+    {
+        return $this->build_query(True);
+    } // }}}
+
+    function build_query($count=False) // {{{
+    {
+        // intended to be overridden to allow use of alternative backends
+        $query = array();
+        $params = array();
+        $this->_max_existing = array();
+        foreach ($this->_filters as &$filter)
+        {
+            // TODO - hide this from the developer
+            list($fquery, $fparams) = $this->_disambiguate_params($params,
+                $filter->params(), $filter->to_string());
+            $query[] = $fquery;
+            $params = array_merge($params, $fparams);
+        }
+        // TODO - extend to allow OR
+        $query = join(' AND ', $query);
+        $fields = '*';
+        if ($count)
+        {
+            $fields = 'COUNT(*) AS _total';
+        }
+        $sql = <<<SQL
+            SELECT {$fields}
+            FROM {$this->_table}
+SQL;
+        if ($query)
+        {
+            $sql .= <<<SQL
+            WHERE {$query}
+SQL;
+        }
+        if (!$count)
+        {
+            $sorting = array();
+            foreach ($this->_sorting as $order_by)
+            {
+                list($field, $direction) = $order_by;
+                if ($direction == '+')
+                {
+                    $direction = 'ASC';
+                }
+                if ($direction == '-')
+                {
+                    $direction = 'DESC';
+                }
+                // TODO - implement random sort
+                $sorting[] = "$field $direction";
+            }
+            if ($sorting)
+            {
+                $sorting = join(', ', $sorting);
+                $sql .= <<<SQL
+                ORDER BY {$sorting}
+SQL;
+            }
+            if ($this->_num)
+            {
+                $sql .= ' LIMIT ';
+                if ($this->_start)
+                {
+                    $sql .= "{$this->_start}, ";
+                }
+                $sql .= $this->_num;
+            }
+        }
+        $sql = trim(preg_replace('/\s+/', ' ', $sql));
+        return array($sql, $params);
+    } // }}}
+
+    function execute_query($query, $params) // {{{
+    {
+        // intended to be overridden to allow use of alternative backends
+        $s = minim('db')->prepare($query);
+        $s->execute($params);
+        return $s;
+    } // }}} 
+
+    function _results_to_objects($s) // {{{
+    {
+        $objects = array();
+        $model =& minim('orm')->{$this->_model};
+        foreach ($s->fetchAll() as $row)
+        {
+            $objects[] =& $model->from($row);
+        }
+        return $objects;
+    } // }}}
+
+    function __get($name) // {{{
+    {
+        // reveal intent
+        if ($name == 'first')
+        {
+            if (!$this->_cache)
+            {
+                $this->_fill_cache();
+            }
+            if (sizeof($this->_cache) < 1)
+            {
+                return NULL;
+            }
+            return $this->_cache[0];
+        }
+    } // }}}
+
+    function to_array() // {{{
+    {
+        $a = array();
+        foreach ($this->items as $item)
+        {
+            $a[] = $item->to_array();
+        }
+        return $a;
+    } // }}}
+
+    // iterator methods
+    var $_iterator_position;
+    function &current() // {{{
+    {
+        return $this->_cache[$this->_iterator_position];
+    } // }}}
+
+    function key() // {{{
+    {
+        return $this->_iterator_position;
+    } // }}}
+
+    function next() // {{{
+    {
+        $this->_iterator_position++;
+    } // }}}
+
+    function rewind() // {{{
+    {
+        $this->_iterator_position = 0;
+    } // }}}
+
+    function valid() // {{{
+    {
+        if (!$this->_cache)
+        {
+            $this->_fill_cache();
+        }
+        if (sizeof($this->_cache) < 1)
+        {
+            return FALSE;
+        }
+        return $this->_iterator_position < count($this->_cache);
+    } // }}}
+} // }}}
+
+class Minim_Orm_Filter // {{{
+{
+    function register() // {{{
+    {
+        // static method, should be overridden
+        // returns an array of associative arrays in the form:
+        // array(<string> => <classname>)
+        // where <string> is the filter query keyword pattern
+        $ops = array(
+            'eq',
+            'ne',
+            'gt',
+            'gte',
+            'lt',
+            'lte',
+            'range',
+        );
+        return array_combine($ops, array_fill(0, count($ops), 'Minim_Orm_Filter'));
+    } // }}}
+
+    function Minim_Orm_Filter($field, $op, $arg) // {{{
+    {
+        $this->field = $field;
+        $this->operator = $op;
+        $this->value = $arg;
+    } // }}}
+
+    function to_string() // {{{
+    {
+        $ops = array(
+            'eq' => '%s = :%s',
+            'ne' => 'NOT (%s = :%s)',
+            'gt' => '%s > :%s',
+            'gte' => '%s >= :%s',
+            'lt' => '%s < :%s',
+            'lte' => '%s <= :%s',
+            'range' => '%s BETWEEN :%s AND :%s',
+        );
+        if ($this->operator == 'range')
+        {
+            return sprintf($ops[$this->operator], $this->field, 'from', 'to');
+        }
+        return str_replace('%s', $this->field, $ops[$this->operator]);
+    } // }}}
+
+    function params() // {{{
+    {
+        if ($this->operator == 'range' and is_array($this->value))
+        {
+            // this is a range value
+            return array(":from" => $this->value[0],
+                         ":to" => $this->value[1]);
+        }
+        return array(":{$this->field}" => $this->value);
+    } // }}}
+} // }}}
+
 class Minim_Orm implements Minim_Plugin // {{{
 {
     var $_models;
@@ -724,6 +1058,6 @@ class Minim_Orm implements Minim_Plugin // {{{
 
     function foreignKey($params=array()) // {{{
     {
-        return array('BreveForeignKey' => $params);
+        return array('Minim_Orm_ForeignKey' => $params);
     } // }}}
 } // }}}

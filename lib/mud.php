3bff196f95cb5ca2b31b876b178ad6c17db1660a
Andy Driver <andrewhdriver@yahoo.com>
2009-01-09 18:10:42 +0000

    Removed old lib stuff

--- lib/mud.php
+++
@@ -1,9 +0,0 @@
-<?php
-function update_timestamp()
-{
-    // set the user's last update time to now
-    $ms = microtime();
-    $cs = (int)($ms * 1000);
-    $now = date('YmdHis') . str_pad($cs, 3, '0', STR_PAD_LEFT);
-    return $now;
-}

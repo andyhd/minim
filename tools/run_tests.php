54a87615cfbedea85d9368a90988195c78f93de9
Andy Driver <andy@andy-nc10.(none)>
2009-03-28 23:27:55 +0000

    Updated to use refactored path()

--- tools/run_tests.php
+++ tools/run_tests.php
@@ -4,5 +4,5 @@ error_reporting(E_STRICT|E_ALL);
 require_once 'minim/minim.php';
 
 // run all tests
-minim('tests')->run_tests(build_path(dirname(__FILE__), '..'));
+minim('tests')->run_tests(path(dirname(__FILE__), '..'));
 minim('tests')->output_results();

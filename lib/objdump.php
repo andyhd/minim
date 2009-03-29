60c192f12d421fa5d974624f3d071cd2a8e67d87
Andy Driver <andy@andy-nc10.(none)>
2009-03-29 18:16:31 +0100

    Added a couple of handy functions for dumping objects to errorlog

---
+++ lib/objdump.php
@@ -0,0 +1,47 @@
+<?php
+$dump_already_seen = array();
+
+function dump($var, $recurse_depth=0)
+{
+    global $dump_already_seen;
+
+    $out = '';
+    if (is_object($var))
+    {
+        $hash = md5($var);
+        $class = get_class($var);
+        $out = "$class object: {";
+        if (in_array($hash, $dump_already_seen))
+        {
+            $out .= '**RECURSION**';
+        }
+        else
+        {
+            $dump_already_seen[] = $hash;
+            $i = 0;
+            foreach ($var as $key => $val)
+            {
+                if ($key[0] != '_')
+                {
+                    $out .= $i++ ? ', ' : '';
+                    $out .= "$key: ".dump($val, $recurse_depth+1);
+                }
+            }
+        }
+        $out .= '}';
+    }
+    else
+    {
+        $out = var_export($var, TRUE);
+    }
+    if ($recurse_depth == 0)
+    {
+        $dump_already_seen = array();
+    }
+    return $out;
+}
+
+function indent($str)
+{
+    return preg_replace('/^/m', '    ', $str);
+}
